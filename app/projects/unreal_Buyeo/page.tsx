b7f5fa9195f541d55737d447b6176d6454293167
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2024-04-19 12:17:05 +0900

    0419 기여도 수정

---
+++ app/projects/unreal_Buyeo/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import FeedDetailNobroker from "@/contents/FeedDetailNobroker";
+import React from "react";
+
+const page = async (props: any) => {
+
+  return (
+    <>
+      <FeedDetailNobroker />
+    </>
+  );
+};
+
+export default page;
--- contents/Role.tsx
+++ contents/Role.tsx
@@ -5,7 +5,7 @@ const Role = ({ myRole }: any) => {
     <>
       <div className="mt-[16px] w-full h-full">
         <h5 className="text-[20px]  w-full">
-          <strong>본인 역할</strong>
+          <strong>담당역할</strong>
         </h5>
         <span className="text-[16px] marginTopGlobalCSS   ">{myRole}</span>
       </div>
